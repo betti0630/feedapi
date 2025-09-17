fe9ab34b79e441f6920fe3f060d49a8d14eccbd4
Beáta Dudás <beata.dudas@gmail.com>
2025-09-17 14:28:42 +0200

    Cleaning

--- AttrectoTest.AppHost/AppHost.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.AttrectoTest_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
-
-builder.Build().Run();
