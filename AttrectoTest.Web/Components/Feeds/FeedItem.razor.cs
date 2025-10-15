7211c4af7bd2cf274043f29ef411e9b1d1b75c7a
Beata Dudas <beata.dudas@gmail.com>
2025-10-15 13:52:36 +0200

    FeedList

---
+++ AttrectoTest.Web/Components/Feeds/FeedItem.razor.cs
@@ -0,0 +1,11 @@
+﻿using AttrectoTest.Web.Services.Base;
+
+using Microsoft.AspNetCore.Components;
+
+namespace AttrectoTest.Web.Components.Feeds;
+
+public partial class FeedItem : ComponentBase
+{
+    [Parameter]
+    public FeedDto? Feed { get; set;}
+}
