4cef6052e97cb37219658335ae7e525c752c8fa7
Beata Dudas <beata.dudas@gmail.com>
2025-10-18 10:27:55 +0200

    Aim service

---
+++ AttrectoTest.AimService/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using AttrectoTest.Application.Contracts.Identity;
+using AttrectoTest.Application.Identity.Dtos.Auth;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttrectoTest.ApiService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IAuthService authService, IAppUserService userService) : ControllerBase
+{
+
+    /// <summary>
+    /// Login (JWT)
+    /// </summary>
+    /// <returns>Successful login</returns>
+    [HttpPost, Route("login", Name = "login")]
+    [AllowAnonymous]
+    public async Task<ActionResult<LoginResponse>> Login([FromBody]LoginRequest request, CancellationToken cancellationToken)
+    {
+        if (!await authService.ValidateUser(request.UserName, request.Password, cancellationToken))
+        {
+            return Unauthorized();
+        }
+        var (token, expires) = await authService.GenerateJwtToken(request.UserName);
+        return Ok(new LoginResponse(token));
+    }
+
+    /// <summary>
+    /// UserDto registration
+    /// </summary>
+    /// <returns>Created</returns>
+    [HttpPost, Route("register", Name = "register")]
+    public async Task<ActionResult<string>> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken)
+    {
+        await userService.AddNewUser(request.UserName, request.Password, "User", cancellationToken);
+        return CreatedAtRoute("register", request.UserName, "User created");    
+    }
+
+}
+
+
---
+++ AttrectoTest.AimService/Program.cs
@@ -0,0 +1,104 @@
+using AttrectoTest.Persistence;
+using AttrectoTest.Application;
+using AttrectoTest.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+
+using Serilog;
+using AttrectoTest.AimService.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig
+    .WriteTo.Console()
+    .ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddInfrastructureServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+
+builder.Services.AddMemoryCache();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+if (allowedOrigins != null && allowedOrigins.Length > 0)
+{
+    builder.Services.AddCors(opt =>
+    {
+        opt.AddPolicy("web", p => p
+        .WithOrigins(allowedOrigins)
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+    });
+}
+
+builder.Services.AddAppAuthentication(builder.Configuration);
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+//builder.Services.AddFastEndpoints();
+
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = "AttrectoTest API";
+    config.Version = "v1";
+
+    // Security Definition (Bearer JWT)
+    config.AddSecurity("JWT", new NSwag.OpenApiSecurityScheme
+    {
+        Type = NSwag.OpenApiSecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = NSwag.OpenApiSecurityApiKeyLocation.Header,
+        Description = "Type into the textbox: Bearer {your JWT token}."
+    });
+
+    // Security Requirement (apply globally)
+    config.OperationProcessors.Add(
+        new NSwag.Generation.Processors.Security.AspNetCoreOperationSecurityScopeProcessor("JWT"));
+});
+
+var app = builder.Build();
+
+
+app.UseCors("web");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+//app.UseExceptionHandler();
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi();
+
+    // Add ReDoc UI to interact with the document
+    // Available at: http://localhost:<port>/redoc
+    app.UseReDoc(options =>
+    {
+        options.Path = "/redoc";
+    });
+}
+app.MapControllers();
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+app.UseStaticFiles();
+
+app.Services.RunDatabaseMigrations();
+
+app.Run();
